8167ef4b1a8543cd0731ac7de62182a06174b6a9
Eamon <38026893+ScottyAndTheNinjas@users.noreply.github.com>
2018-05-24 09:04:32 +1000

    Updated Template
    Added work we did on 24/05

--- template.py
+++ template.py
@@ -3,10 +3,16 @@ A longer description of the module with details that may help the user or anybod
 reviewing the code later. make sure you outline in detail what the module does and how it can be used.
 """
 
-__author__ = "Eamon Petersons, Zyon Shepherd, ThomasRampart"
+__author__ = "Eamon Petersons, Zyon Shepherd, Thomas Rampart"
 __copyright__ = "Copyright 2018, NSW Dept. Ed."
 __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = ""
 __email__ = "eamon.petersons@education.nsw.go.au"
 __status__ = "Prototype"
+
+
+
+
+level = input("Welcome to Hangman! You have a choice of 4 levels. These levels are easy, medium, hard and unique. What level would you like to play? ")
+              
